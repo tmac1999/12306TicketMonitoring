0e97f95790e436043125fad291db14fc97a04bad
tmac1999 <119141701@qq.com>
2017-01-10 20:56:24 +0800

    出发到达城市的选择。

--- app/src/main/java/com/mrz/autorefreshtickets/MainActivity.java
+++ app/src/main/java/com/mrz/autorefreshtickets/MainActivity.java
@@ -4,20 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
+import com.mrz.autorefreshtickets.utils.SharedPUtil;
 import com.mrz.autorefreshtickets.viewinferface.MainView;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.https.HttpsUtils;
 
 import okhttp3.OkHttpClient;
 
-public class MainActivity extends AppCompatActivity implements MainView {
-    String url = "https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=2017-01-23&leftTicketDTO.from_station=BJP&leftTicketDTO.to_station=WHN&purpose_codes=ADULT";
+public class MainActivity extends AppCompatActivity implements MainView, View.OnClickListener {
+    String url = "https://kyfw.12306.cn/otn/leftTicket/queryA" +
+            "?leftTicketDTO.train_date=" +
+            "2017-01-23" +
+            "&leftTicketDTO.from_station=" +
+            "BJP" +
+            "&leftTicketDTO.to_station=" +
+            "WHN" +
+            "&purpose_codes=ADULT";
+    private Button btn_start_query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_start_query = (Button) findViewById(R.id.btn_start_query);
+        btn_start_query.setOnClickListener(this);
         HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(null, null, null);
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager)
@@ -52,5 +64,17 @@ public class MainActivity extends AppCompatActivity implements MainView {
 //                Toast.makeText(MainActivity.this, "ddddddd", Toast.LENGTH_SHORT).show();
 //            }
 //        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start_query:
+                String arrival = SharedPUtil.getInstance().getArrival();
+                String departure = SharedPUtil.getInstance().getDeparture();
+                String date = SharedPUtil.getInstance().getDate();
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/mrz/autorefreshtickets/RefreshSettingActivity.java
+++ app/src/main/java/com/mrz/autorefreshtickets/RefreshSettingActivity.java
@@ -3,29 +3,37 @@ package com.mrz.autorefreshtickets;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.CalendarView;
-import android.widget.EditText;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.mrz.autorefreshtickets.presenters.RefreshSettingPresenter;
+import com.mrz.autorefreshtickets.utils.Constants;
 import com.mrz.autorefreshtickets.utils.SharedPUtil;
 import com.mrz.autorefreshtickets.utils.TimeUtils;
 import com.mrz.autorefreshtickets.utils.ToastUtils;
 import com.mrz.autorefreshtickets.viewinferface.RefreshSettingView;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class RefreshSettingActivity extends AppCompatActivity implements RefreshSettingView {
     // Content View Elements
 
-    private EditText et_departure;
-    private EditText et_arrival;
+    private AutoCompleteTextView et_departure;
+    private AutoCompleteTextView et_arrival;
     private CalendarView calendar;
     private RefreshSettingPresenter refreshSettingPresenter;
+    private JSONObject jsonObject;
 
     // End Of Content View Elements
 
     private void bindViews() {
 
-        et_departure = (EditText) findViewById(R.id.et_departure);
-        et_arrival = (EditText) findViewById(R.id.et_arrival);
+        et_departure = (AutoCompleteTextView) findViewById(R.id.aTv_departure);
+        et_arrival = (AutoCompleteTextView) findViewById(R.id.aTv_arrival);
         calendar = (CalendarView) findViewById(R.id.calendar);
     }
 
@@ -39,15 +47,30 @@ public class RefreshSettingActivity extends AppCompatActivity implements Refresh
     }
 
     private void initData() {
+        ArrayList<String> stations = new ArrayList<>();
         et_arrival.setText(SharedPUtil.getInstance().getArrival());
         et_departure.setText(SharedPUtil.getInstance().getDeparture());
+        jsonObject = (JSONObject) JSON.parse(Constants.stations);
+        Set<String> keys = jsonObject.keySet();
+        for (String key : keys) {
+            stations.add(key);
+        }
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stations);
+
+
+        et_departure.setAdapter(arrayAdapter);
+        et_arrival.setAdapter(arrayAdapter);
     }
 
     public void save(View v) {
         String arrival = et_arrival.getText().toString();
         String departure = et_departure.getText().toString();
+        String arrivalShort = null;
+        String departureShort = null;
+        arrivalShort = (String) jsonObject.get(arrival);
+        departureShort = (String) jsonObject.get(departure);
         String dateFromTimeStampMS = TimeUtils.getDateFromTimeStampMS(calendar.getDate());
-        refreshSettingPresenter.saveRefreshSetting(arrival, departure, dateFromTimeStampMS);
+        refreshSettingPresenter.saveRefreshSetting(arrivalShort, departureShort, dateFromTimeStampMS);
         ToastUtils.showLongMessage("保存成功");
         finish();
     }
